b8e410ba97730f71b0d3014a681461afa06ee8b9
cgc186 <985305715@qq.com>
2019-10-08 22:42:56 +0800

    hough直线检测

---
+++ src/main/kotlin/second/hough/Main.kt
@@ -0,0 +1,30 @@
+package second.hough
+
+import java.awt.FileDialog
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+import javax.swing.JFrame
+import kotlin.system.exitProcess
+
+fun main() {
+    //val lineFilter= LineFilter()
+
+    val houghLine= HoughLine()
+
+    val jf = JFrame()
+    val fd = FileDialog(jf,"选择图片")
+    fd.show()
+    val img = ImageIO.read(File(fd.directory,fd.file))
+    //val resulting = lineFilter.lineFilter(img)
+
+    val resulting = houghLine.hough(img)
+
+    val output = BufferedImage(img.width, img.height, BufferedImage.TYPE_INT_BGR)
+    output.createGraphics().drawImage(resulting, 0, 0, null)
+    val fd2 = FileDialog(jf,"储存图片")
+    fd2.mode = FileDialog.SAVE
+    fd2.show()
+    ImageIO.write(resulting,"jpg", File(fd2.directory,fd2.file))
+    exitProcess(0)
+}
